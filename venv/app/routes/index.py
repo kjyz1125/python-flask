f76001382c34c7a1367064f3c776b3c951a5c407
kjyz1125 <kjyz1125@gmail.com>
2018-02-03 17:24:47 +0900

    index page commit test

--- venv/app/routes/index.py
+++ venv/app/routes/index.py
@@ -1,9 +1,9 @@
 from app import app
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 @app.route('/')
 def main():
-    return "hello world"
+    return render_template('index.html')
 
 
 @app.route('/data')
