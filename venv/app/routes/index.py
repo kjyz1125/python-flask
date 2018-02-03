8027249863e11fdfbd40c0dff556e328539b59a7
kjyz1125 <kjyz1125@gmail.com>
2018-02-03 16:31:59 +0900

    git first test

--- venv/app/routes/index.py
+++ venv/app/routes/index.py
@@ -1,6 +1,12 @@
 from app import app
-from flask import Flask
+from flask import Flask, jsonify
 
 @app.route('/')
 def main():
     return "hello world"
+
+
+@app.route('/data')
+def data():
+    data = {"test": "test"}
+    return jsonify(data)
