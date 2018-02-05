ca4c72d0317b6ac7f04e9a3882b9b4ddf858028b
kjyz1125 <kjyz1125@gmail.com>
2018-02-05 16:02:50 +0900

    한글 처리 완료

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+install_requires = [
+    'Flask==0.12.2',
+    'Jinja2==2.10',
+    'MarkupSafe==1.0',
+    'SQLAlchemy==1.2.2',
+    'Werkzeug==0.14.1',
+    'click==6.7',
+    'itsdangerous==0.24',
+    'mysql-connector-python==8.0.6',
+    'pip==9.0.1',
+    'setuptools==28.8.0'
+    ]
+
+setup(
+    name='python-flask',
+    version='0.1',
+    packages=['app'],
+    url='',
+    license='',
+    author='kjyz1125',
+    author_email='',
+    description='',
+    include_package_data=True,
+    install_requires=install_requires
+)
