5149be3e1ff3f54da3d234d07d2b63ca5a561ae8
kjyz1125 <kjyz1125@gmail.com>
2018-02-05 17:26:59 +0900

    주소 삭제

--- venv/app/database/database.py
+++ venv/app/database/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('mysql+mysqlconnector://root:vhxkf!111@13.125.143.200:3306/part1?charset=utf8', convert_unicode=True)
+engine = create_engine('?', convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 Base = declarative_base()
 Base.query = db_session.query_property()
